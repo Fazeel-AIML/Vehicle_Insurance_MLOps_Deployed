815a8269790ae35093977929aecf9a259271bd67
Fazeel-AIML <fasghar40@gmail.com>
2025-02-23 10:22:48 +0500

    Added logger and exceptions

---
+++ demo.py
@@ -0,0 +1,5 @@
+from src.logger import logging
+
+logging.debug("This is debug mode")
+logging.error("Ths is error")
+logging.critical("This is critical")
---
+++ src/logger/__init__.py
@@ -0,0 +1,34 @@
+import logging
+from from_root import from_root
+import os
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+
+LOG_DIR = "logs"
+LOG_FILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%S")}.log"
+MAX_LOG_FILE = 5*1024*1024 #5MB
+BACKUP_LOGS = 3
+
+log_dir_path = os.path.join(from_root(),LOG_DIR)
+os.makedirs(log_dir_path,exist_ok=True)
+log_file_path = os.path.join(log_dir_path,LOG_FILE)
+
+def configure_logger():
+    
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    
+    formatter = logging.Formatter("[%(asctime)s] %(name)s - %(levelname)s %(message)s")
+    
+    filehandler= RotatingFileHandler(log_file_path,maxBytes=MAX_LOG_FILE,backupCount=BACKUP_LOGS)
+    filehandler.setFormatter(formatter)
+    filehandler.setLevel(logging.DEBUG)
+    
+    console_handler =  logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel("DEBUG")
+    
+    logger.addHandler(filehandler)
+    logger.addHandler(console_handler)
+    
+configure_logger()
