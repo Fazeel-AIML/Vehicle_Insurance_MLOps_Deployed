a1c868f16876e109151587b1fb902acf4b0ed9e3
Fazeel-AIML <fasghar40@gmail.com>
2025-02-26 23:40:54 +0500

    Debug somethng

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 from datetime import datetime
 
 LOG_DIR = "logs"
-LOG_FILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%S")}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%S')}.log"
 MAX_LOG_FILE = 5*1024*1024 #5MB
 BACKUP_LOGS = 3
 
